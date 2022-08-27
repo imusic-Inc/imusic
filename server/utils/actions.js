22f2bf7617901b35ff6274fb028fd573c1a2ac88
JudeEdemNyahe <97879372+JudeEdemNyahe@users.noreply.github.com>
2022-08-27 23:11:16 +0100

    authorization user roles

---
+++ server/utils/actions.js
@@ -0,0 +1,9 @@
+const ACTIONS = {
+    JOIN: 'join',
+    LEAVE: 'leave',
+    ADD_PEER: 'add-peer',
+    REMOVE_PEER: 'remove-peer',
+    SESSION_DESCRIPTION: 'session-description'
+};
+
+module.exports = ACTIONS;

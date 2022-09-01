27fc28e2732a1e475bb3d0e38261a781a34c67c6
JudeEdemNyahe <97879372+JudeEdemNyahe@users.noreply.github.com>
2022-09-01 15:41:47 +0100

    model and scopes update

--- server/models/PrivateConversation.js
+++ server/models/PrivateConversation.js
@@ -4,6 +4,7 @@ const ConversationSchema = new mongoose.Schema({
     members: {
         type: Array,
     },
+
 }, { timestamps: true })
 
 module.exports = mongoose.model("Conversation", ConversationSchema)

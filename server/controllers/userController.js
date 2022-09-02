374ae70ceb87453070831fd290a3c80036ad296b
JudeEdemNyahe <97879372+JudeEdemNyahe@users.noreply.github.com>
2022-09-02 16:15:46 +0100

    session update route and api features for users

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,10 +1,18 @@
+const APIFeatures = require("../utils/apiFeatures");
 const User = require("./../models/userModel");
 const hookAsync = require('./../utils/hookAsync');
 
 
 exports.getAllUsers = hookAsync(async(req, res, next) => {
 
-    const users = await User.find();
+    const features = new APIFeatures(User.find(), req.query)
+        .filter()
+        .sort()
+        .limitFields()
+        .paginate();
+
+    const users = await features.query;
+
     res.status(200).json({
         status: 'success',
         results: users.length,

4aa03b145779011c8565a8c7903e60645a3f2bcd
John Tekpor Dzikunu <john.dzikunu@amalitech.org>
2022-09-09 14:17:35 +0000

    improved shares

--- client/src/session/room.jsx
+++ client/src/session/room.jsx
@@ -91,8 +91,7 @@ const [expandInvite, setExpandInvite] = useState(false);
         setGuest(value.guest);
         setOwerId(value.ownerId);
         setCurrent([value.now_playing]);
-        const allow = new Boolean(value.allowInvite);
-        setAllowInvite(allow);
+        setAllowInvite(value.allowInvite ? value.allowInvite : true);
         link = '../room/' + value.id + '?name=' + value.name + '&admin=true&type=' + value.roomType;
     }
 
@@ -223,7 +222,7 @@ pauseOnHover
     <path fill="currentColor" d="M20 14H14V20H10V14H4V10H10V4H14V10H20V14Z" />
 </svg>
                 </div>
- <div className='btn btn-back flex-row flex-center p-01' onClick={allowInvite && uid && uid.length > 10 ?showAndHideInvite:notify}>
+ <div className='btn btn-back flex-row flex-center p-01' onClick={type !=='private'?showAndHideInvite:allowInvite?showAndHideInvite:uid ===owerId?showAndHideInvite:notify}>
                     <svg style={{ width: '24px', height: '24px' }} viewBox="0 0 24 24">
     <path fill="currentColor" d="M19 17V19H7V17S7 13 13 13 19 17 19 17M16 8A3 3 0 1 0 13 11A3 3 0 0 0 16 8M19.2 13.06A5.6 5.6 0 0 1 21 17V19H24V17S24 13.55 19.2 13.06M18 5A2.91 2.91 0 0 0 17.11 5.14A5 5 0 0 1 17.11 10.86A2.91 2.91 0 0 0 18 11A3 3 0 0 0 18 5M8 10H5V7H3V10H0V12H3V15H5V12H8Z" />
 </svg>

d200eb6ab0a1e4580f59c4618f5d774c6af97147
JudeEdemNyahe <97879372+JudeEdemNyahe@users.noreply.github.com>
2022-09-11 21:35:57 +0000

    content field for notification model

--- server/models/notification.js
+++ server/models/notification.js
@@ -7,16 +7,18 @@ const NotificationSchema = new Schema({
     userTo: { type: Schema.Types.ObjectId, ref: 'User' },
     userFrom: { type: Schema.Types.ObjectId, ref: 'User' },
     notificationType: String,
+    content: String,
     opened: { type: Boolean, default: false },
     entityId: Schema.Types.ObjectId
 }, { timestamps: true })
 
 
-NotificationSchema.statics.insertNotification = async(userTo, userFrom, notificationType, entityId) => {
+NotificationSchema.statics.insertNotification = async(userTo, userFrom, notificationType, content, entityId) => {
     const data = {
         userTo,
         userFrom,
         notificationType,
+        content,
         entityId
     };
 

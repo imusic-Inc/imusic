a09c1b27713729880f2ac7ae2732490cf346ff5d
John Tekpor Dzikunu <john.dzikunu@amalitech.org>
2022-09-14 20:04:57 +0000

    cleaning css and minor bugs fixed

--- client/src/api/keys.js
+++ client/src/api/keys.js
@@ -2,7 +2,7 @@ const keys = {
     "Client_ID": "42d997ba7e5f410d90580f84b4a4d29b",
     "Client_Secret": "d85d43e05b4446ec860c85444881c408",
     "SERVER": 'https://amalitech-imusic.herokuapp.com',
-    "DOMAIN": 'https://imusicroom.netlify.app'
+    "DOMAIN": 'https://imusicroom.herokuapp.com'
     
 }
 
